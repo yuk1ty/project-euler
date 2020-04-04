dce7c25994064c2db3076398079c06e40f7964a2
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-04-04 17:09:15 +0900

    Faster version for problem3

--- src/bin/problem3.rs
+++ src/bin/problem3.rs
@@ -1,40 +1,21 @@
 fn main() {
-    // brute_force can't over!
-    println!("{}", biggest_prime_factor_brute_force(600851475143));
+    println!("{}", biggest_prime_factor(600851475143));
 }
 
-fn is_prime(n: i64) -> bool {
-    if n == 0 || n == 1 || is_even(n) {
-        return false;
-    }
-
-    for i in 3..((n as f64).sqrt() as i64) {
-        if is_even(n) {
-            continue;
-        }
-        if n as f64 % i as f64 == 0.0 {
-            return false;
-        }
-    }
-    true
-}
-
-fn is_even(n: i64) -> bool {
-    n & 1 == 0
-}
-
-fn biggest_prime_factor_brute_force(n: i64) -> i64 {
-    let mut biggest = 0;
-
-    for i in 2..n {
-        if n as f64 % i as f64 == 0.0 {
-            if is_prime(i) {
-                if i > biggest {
-                    biggest = i;
-                }
+fn biggest_prime_factor(m: i64) -> i64 {
+    let mut n: i64 = m;
+    for p in 2..n {
+        if p * p > n {
+            break;
+        } else if n % p == 0 {
+            n /= p;
+            while n % p == 0 {
+                n /= p;
+            }
+            if n == 1 {
+                return p;
             }
         }
     }
-
-    biggest
+    return n;
 }
